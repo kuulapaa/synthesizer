6a49e6096a6c2248c9ee0b4562820fcb5c4bc57a
kuulapaa <45540438+kuulapaa@users.noreply.github.com>
2018-12-29 16:02:42 +0200

    Update generators_test.ino

--- generators_test.ino
+++ generators_test.ino
@@ -2,10 +2,13 @@
 uint16_t freqs[] = {
  4186,4435,4699,4978,5274,5588,5920,6272,6645,7040,7459,7902 }; // twice the frequencies of 7th octave
  
+ 
  // Real piano 7th octave frequencies (about), bit shift right to get 7 and so on...
  // 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951};
 
-#define OSCILLATORS 8
+#define OSCILLATORS 1
+#define SAW 1
+#define TRI 2
 
 volatile struct voices {
   uint16_t freq;
@@ -14,12 +17,13 @@ volatile struct voices {
   uint8_t volume;
 } gens[OSCILLATORS];
 
+
+/*
 enum {
   TRIANGLE,
   SAW,
 };
-
-uint8_t i;
+*/
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,8 +33,11 @@ void setup() {
 }
 
 void setup_voices() { // FOR TESTING
-  gens[0].freq = freqs[0]>>4;
-  gens[0].wf = SAW;
+  gens[0].freq = freqs[0]>>2;
+  gens[0].wf = TRI;
+
+  gens[1].freq = freqs[2]>>3;
+  gens[1].wf = 4;
 }
 
 void timer0_init()
@@ -49,73 +56,23 @@ void loop(void)
 ISR(TIMER0_COMPA_vect)
 {
   uint16_t output;
-  for (i=0; i<OSCILLATORS; i++) { 
-    switch(gens[i].wf) {
-      case TRIANGLE:
+  for (uint8_t i=0; i<OSCILLATORS; i++) { 
+    if ((gens[i].wf) == TRI) {
       gens[i].phase += gens[i].freq;  // Add frequency value in every interrupt. Bit shift right gives lower octave
-      if (gens[i].phase > 32768) output += (((0xFFFF ^ gens[i].phase)<<1)>>3); // XOR to ramp down
-      else output += ((gens[i].phase<<1)>>3);
-      break;
-
-      case SAW:
+      if (gens[i].phase & _BV(15)) {
+      //if (gens[i].phase > 32768) {
+        output += (((0xFFFF ^ gens[i].phase)<<1)>>3); // XOR to ramp down
+      }
+      else {
+        output += ((gens[i].phase<<1)>>3);
+      }
+    }
+    
+    if ((gens[i].wf) == SAW) {
       gens[i].phase += gens[i].freq;
       output += ((gens[i].phase)>>3);
-      break;
-
-      default:
-      break;
     }
+    else {}
   }
   PORTD = (output>>8);  // Just output the damn thing and bit shift so it can fit 8 bits
 }
-
-/*
-ISR(TIMER0_COMPA_vect)
-{
-    //TRIANGLE GEN
-    uint16_t output = 0;
-    
-    triangle += (freqs[0]>>4);  // Add frequency value in every interrupt. Bit shift right gives lower octave
-    if (triangle > 32768) triangle_out = ((0xFFFF ^ triangle)<<1); // XOR to ramp down
-    else triangle_out = (triangle<<1);
-    output = (triangle_out>>3);  //
-    
-    //TRIANGLE GEN2
-    triangle2 += (freqs[1]>>3);
-    if (triangle2 > 32768) triangle2_out = ((0xFFFF ^ triangle2)<<1);
-    else triangle2_out = (triangle2<<1);
-    output += (triangle2_out>>3);
-
-    //TRIANGLE GEN3
-    triangle3 += (freqs[4]>>5);
-    if (triangle3 > 32768) triangle3_out = ((0xFFFF ^ triangle3)<<1);
-    else triangle3_out = (triangle3<<1);
-    output += (triangle3_out>>3);
-
-    //TRIANGLE GEN4
-    triangle4 += (freqs[5]>>4);
-    if (triangle4 > 32768) triangle4_out = ((0xFFFF ^ triangle4)<<1);
-    else triangle4_out = (triangle4<<1);
-    output += (triangle4_out>>3);
-    
-    //SAW GEN
-    sawtooth += (freqs[2]>>2);
-    output += (sawtooth>>3);
-
-    //SAW GEN2
-    sawtooth2 += freqs[3]>>2;
-    output += (sawtooth2 >> 3);
-
-    //SAW GEN3
-    sawtooth3 += freqs[4]>>5;
-    output += (sawtooth3 >> 3);
-
-     //SAW GEN4
-    sawtooth4 += freqs[6]>>7;
-    output += (sawtooth4 >> 3);
-
-
-
-    PORTD = (output>>8);  // Just output the damn thing and bit shift so it can fit 8 bits
-}
-*/
