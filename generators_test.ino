d3adeefb9726428b2dacceffb7d88c39571c0ad8
kuulapaa <45540438+kuulapaa@users.noreply.github.com>
2018-12-29 21:00:45 +0200

    Update generators_test.ino

--- generators_test.ino
+++ generators_test.ino
@@ -6,9 +6,14 @@ uint16_t freqs[] = {
  // Real piano 7th octave frequencies (about), bit shift right to get 7 and so on...
  // 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951};
 
-#define OSCILLATORS 1
-#define SAW 1
-#define TRI 2
+#define OSCILLATORS 2
+#define ATTEN 2
+#define MOD_O 1   // oscillator generating the ring modulation
+#define RINGM 1
+
+const uint8_t SAW = 1;
+const uint8_t TRI = 2;
+const uint8_t PUL = 3;
 
 volatile struct voices {
   uint16_t freq;
@@ -33,11 +38,23 @@ void setup() {
 }
 
 void setup_voices() { // FOR TESTING
-  gens[0].freq = freqs[0]>>2;
+  gens[0].freq = freqs[0]>>7;
   gens[0].wf = TRI;
 
-  gens[1].freq = freqs[2]>>3;
-  gens[1].wf = 4;
+  gens[1].freq = freqs[3]>>1;
+  gens[1].wf = SAW;
+
+  gens[2].freq = freqs[1]>>4;
+  gens[2].wf = TRI;
+
+  gens[3].freq = freqs[5]>>5;
+  gens[3].wf = TRI;
+
+  gens[4].freq = freqs[2]>>1;
+  gens[4].wf = PUL;
+
+   gens[5].freq = freqs[2]>>1;
+  gens[5].wf = SAW;
 }
 
 void timer0_init()
@@ -55,24 +72,36 @@ void loop(void)
 
 ISR(TIMER0_COMPA_vect)
 {
-  uint16_t output;
-  for (uint8_t i=0; i<OSCILLATORS; i++) { 
-    if ((gens[i].wf) == TRI) {
-      gens[i].phase += gens[i].freq;  // Add frequency value in every interrupt. Bit shift right gives lower octave
+  uint16_t output = 0;
+  for (uint8_t i=0; i<OSCILLATORS; i++) {
+     
+    if ((gens[i].wf) == TRI) {                                  // Triangle waveform generator, XOR to get ramp down
+      gens[i].phase += gens[i].freq;
+      if (RINGM) {
+        uint16_t bit_mask = (1<<15);                            // use MSB for ring modulation as in real SID
+        uint16_t msb_m = gens[MOD_O].phase & bit_mask;
+        gens[i].phase = (gens[i].phase & (~bit_mask)) | msb_m;  // substitute MSB of oscillator accumulator with MSB of carrier signal MSB
+      }
       if (gens[i].phase & _BV(15)) {
-      //if (gens[i].phase > 32768) {
-        output += (((0xFFFF ^ gens[i].phase)<<1)>>3); // XOR to ramp down
+        output += (((0xFFFF ^ gens[i].phase)<<1)>>ATTEN);
       }
       else {
-        output += ((gens[i].phase<<1)>>3);
+        output += ((gens[i].phase<<1)>>ATTEN);
       }
     }
-    
-    if ((gens[i].wf) == SAW) {
+  
+    if ((gens[i].wf) == SAW) {              // Sawtooth waveform generator
       gens[i].phase += gens[i].freq;
-      output += ((gens[i].phase)>>3);
+      //output += ((gens[i].phase)>>ATTEN);
     }
-    else {}
+    
+    if ((gens[i].wf) == PUL){
+      gens[i].phase += gens[i].freq;        // Pulse waveform generator
+      if (!(gens[i].phase & _BV(15))) {
+        output += (0xFFFF>>ATTEN);
+      }
+    }
   }
-  PORTD = (output>>8);  // Just output the damn thing and bit shift so it can fit 8 bits
+  if (output >= 0xFFF0) while(1) {}         // Halt if output overdriven
+  PORTD = (output>>8);                      // Just output the damn thing and bit shift so it can fit 8 bits
 }
